8d17fbce08083f0ce326f7e2cf122c47a5cf86f9
Kumi Kwasi <141152235+nanakumi75@users.noreply.github.com>
2024-12-07 19:27:38 -0500

    Update database.php

--- functions/database.php
+++ functions/database.php
@@ -1,9 +1,10 @@
 <?php 
-/*define('db_host','localhost');
+
+define('db_host','localhost');
 define('db_user','root');
 define('db_password','');
 define('db_name','econ');
 
 $conn = new mysqli(db_host,db_user,db_password,db_name);
-*/
+
  
